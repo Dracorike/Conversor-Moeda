4091df47157b72bcf0cd1c1b1e80e56f352c139f
Dracorike <pedrohenriqueknd@gmail.com>
2021-10-18 19:28:44 -0300

    pequenas alterações no layout

--- app/src/main/java/com/petruciostech/conversordemoeda/view/MainActivity.kt
+++ app/src/main/java/com/petruciostech/conversordemoeda/view/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.petruciostech.conversordemoeda.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.petruciostech.conversordemoeda.R
 import com.petruciostech.conversordemoeda.databinding.ActivityMainBinding
+import com.petruciostech.conversordemoeda.util.tools.SHARED_BOOLEAN_INITORNOT
+import com.petruciostech.conversordemoeda.util.tools.SHARED_PREF_KEY
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind:ActivityMainBinding
@@ -13,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
+        fragmentReplace(ConvertFragment())
         initComponents()
-        fragmentReplace(MainFragment())
     }
 
     private fun initComponents(){
@@ -26,7 +29,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
-
     private fun fragmentReplace(fragment:Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(bind.containerFragmentswitherFrame.id, fragment)
